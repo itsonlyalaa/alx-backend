2aeaeec2ccd693a1a135cc40d2fd4437efa7760a
Alaa Okair <alaaokair330@gmail.com>
2024-08-24 01:26:49 +0000

    task 1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -14,6 +14,7 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
 
     return (start, final)
 
+
 class Server:
     """Server class to paginate a database of popular baby names.
     """

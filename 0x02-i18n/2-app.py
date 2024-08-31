f6cad719a03263461d2bb792630ce666bb1cd03b
Alaa Okair <alaaokair330@gmail.com>
2024-08-31 02:17:58 +0000

    tasks

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Basic Flask app module"""
 from flask import Flask, render_template
+from flask_babel import Babel
 
 app = Flask(__name__)
 babel = Babel(app)
@@ -19,7 +20,13 @@ app.config.from_object(Config)
 @app.route('/')
 def hello():
     """hello method"""
-    return render_template('1-index.html')
+    return render_template('2-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """method to determine the best match with our supported languages"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == "__main__":
